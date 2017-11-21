c77bff4f7697446362151e9164a0ef43d87e53d8
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-11-21 10:31:19 +0700

    update

--- async.js
+++ async.js
@@ -1,7 +1,12 @@
 const request = require('request');
 
 const test = () => {
-  
+    const requestResponse = (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+    }
+    request('http://www.google.com', requestResponse);
 }
 //START OF PROGRAM
 console.log("Hitting google");
